2d5478ea2ecf35c6dc5a24f2882dda6b23868828
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-02-20 17:57:33 +0100

    feat(package): include pretty, build on changes

--- .next/dist/bundles/pages/index.js
+++ .next/dist/bundles/pages/index.js
@@ -100,49 +100,14 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (typeof window !== 'undefined') {
-  Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["hydrate"])(window.__NEXT_DATA__.ids);
+    Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["hydrate"])(window.__NEXT_DATA__.ids);
 }
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["injectGlobal"])('html,body{padding:3rem 1rem;margin:0;background:papayawhip;min-height:100%;font-family:Helvetica,Arial,sans-serif;font-size:24px;}');
-  var basicStyles = /*#__PURE__*/Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["css"])('background-color:white;color:cornflowerblue;border:1px solid lightgreen;border-right:none;border-bottom:none;box-shadow:5px 5px 0 0 lightgreen,10px 10px 0 0 lightyellow;transition:all 0.1s linear;margin:3rem 0;padding:1rem 0.5rem;');
-  var hoverStyles = /*#__PURE__*/Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["css"])('&:hover{color:white;background-color:lightgray;border-color:aqua;box-shadow:-15px -15px 0 0 aqua,-30px -30px 0 0 cornflowerblue;}');
-  var bounce = /*#__PURE__*/Object(__WEBPACK_IMPORTED_MODULE_1_react_emotion__["keyframes"])('from{transform:scale(1.01);}to{transform:scale(0.99);}');
-  var Basic = /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_1_react_emotion___default()('div', {
-    target: 'css-1u0sjot0'
-  })(basicStyles, ';');
-  var Combined = /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_1_react_emotion___default()('div', {
-    target: 'css-1u0sjot1'
-  })(basicStyles, ';', hoverStyles, ';& code{background-color:linen;}');
-  var Animated = /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_1_react_emotion___default()('div', {
-    target: 'css-1u0sjot2'
-  })(basicStyles, ';', hoverStyles, ';& code{background-color:linen;}animation:', function (props) {
-    return props.animation;
-  }, ' 0.2s infinite ease-in-out alternate;');
-  return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
-    'div',
-    null,
-    __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
-      Basic,
-      null,
-      'Cool Styles'
-    ),
-    __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
-      Combined,
-      null,
-      'With ',
-      __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
-        'code',
+    return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
+        'div',
         null,
-        ':hover'
-      ),
-      '.'
-    ),
-    __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](
-      Animated,
-      { animation: bounce },
-      'Let\'s bounce.'
-    )
-  );
+        'Hello World'
+    );
 });
 
 /***/ }),
